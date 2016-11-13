5c4a0794645aa3ee4407535fb4d1d3735c7afabc
Satyanand-Jha <satyanand.jha@hotmail.com>
2016-11-13 00:29:13 +0000

    fixed bug

--- CSharp/calculateAge.cs
+++ CSharp/calculateAge.cs
@@ -1,12 +1,12 @@
         static string getAge(string dateOfBirthAsString)
         {
-            string _age = "";
+            string _age = "_error";
             if (!string.IsNullOrWhiteSpace(dateOfBirthAsString))
             {
-                int? _years = 0;
-                int? _months = 0;
+                int _years = 0, _months = 0;
                 DateTime _todaysDate = DateTime.Now;
                 DateTime _dateOfBirth = Convert.ToDateTime(dateOfBirthAsString);
+
                 if (_todaysDate > _dateOfBirth)
                 {
                     if (_todaysDate.Month >= _dateOfBirth.Month)
@@ -14,24 +14,32 @@
                         _years = _todaysDate.Year - _dateOfBirth.Year;
                         _months = _todaysDate.Month - _dateOfBirth.Month;
                     }
-                    else
+                    else if (_todaysDate.Month < _dateOfBirth.Month)
                     {
                         _years = (_todaysDate.Year - _dateOfBirth.Year) - 1;
-                        _months = _dateOfBirth.Month - _todaysDate.Month;
-                    }
-                    if (_years > 0)
-                    {
-                        _age = _months == 0 ?
-                               _years.ToString() + " Years" :
-                               _years.ToString() + " Years " + _months + " Months";
-                    }
-                    else
-                    {
-                        _age = _months > 0 ?
-                               "0 Years " + _months.ToString() + " Months" :
-                               "Not Available";
-                    }
-                }               
-            }
+                        _months = 12 - (_dateOfBirth.Month - _todaysDate.Month);
+                    }   
+                    _age = getAgeToDspaly(_years, _months);
+                }                               
+            }           
             return _age;
+        }
+
+        static string getAgeToDspaly(int years, int months)
+        {
+            string _ageToDisplay = "";
+            if (years > 0)
+            {
+                _ageToDisplay = months > 0 ?
+                                years.ToString() + " Years " + months.ToString() + " Months" :
+                            years.ToString() + " Years";
+                                
+            }
+            else
+            {
+                _ageToDisplay = months > 0 ?
+                                "0 Years " + months.ToString() + " Months" :
+                                "Not Available";
+            }
+            return _ageToDisplay;
         }
