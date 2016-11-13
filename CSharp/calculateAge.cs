824fe04bfad15a0a3df5195b78a909a33810e330
Satyanand-Jha <satyanand.jha@hotmail.com>
2016-11-13 19:17:44 +0000

    Revert "Code to calculate age"
    This reverts commit 4fc19cf717a2ad9ebfd5810ff0f19eb21923a775.
    
    # Conflicts:
    #	CSharp/calculateAge.cs

--- CSharp/calculateAge.cs
+++ CSharp/calculateAge.cs
@@ -1,45 +1,45 @@
-        static string getAge(string dateOfBirthAsString)
+          static string getAge(string dateOfBirthAsString)
         {
-            string _age = "_error";
-            if (!string.IsNullOrWhiteSpace(dateOfBirthAsString))
+            string _age = "age_calc_error";
+            DateTime _dateOfBirth;
+            if (!string.IsNullOrWhiteSpace(dateOfBirthAsString) && 
+                 DateTime.TryParse(dateOfBirthAsString, out _dateOfBirth))
             {
-                int _years = 0, _months = 0;
+                int _years = 0, _months = 0, _dayDifference = 0;
                 DateTime _todaysDate = DateTime.Now;
-                DateTime _dateOfBirth = Convert.ToDateTime(dateOfBirthAsString);
-
+                
                 if (_todaysDate > _dateOfBirth)
                 {
-                    if (_todaysDate.Month >= _dateOfBirth.Month)
-                    {
-                        _years = _todaysDate.Year - _dateOfBirth.Year;
-                        _months = _todaysDate.Month - _dateOfBirth.Month;
-                    }
-                    else if (_todaysDate.Month < _dateOfBirth.Month)
-                    {
-                        _years = (_todaysDate.Year - _dateOfBirth.Year) - 1;
-                        _months = 12 - (_dateOfBirth.Month - _todaysDate.Month);
-                    }   
-                    _age = getAgeToDspaly(_years, _months);
-                }                               
-            }           
+                    _dayDifference = _todaysDate.Day - _dateOfBirth.Day;
+
+                    _todaysDate = _dayDifference < 0 ? _todaysDate.AddMonths(-1) : _todaysDate;
+
+                    _years = _todaysDate.Month >= _dateOfBirth.Month ?
+                             _todaysDate.Year - _dateOfBirth.Year :
+                             _todaysDate.Year - _dateOfBirth.Year - 1;
+
+                    _months = _todaysDate.Month >= _dateOfBirth.Month ?
+                             (_todaysDate.Month - _dateOfBirth.Month) :
+                             (12 - (_dateOfBirth.Month - _todaysDate.Month));
+                }
+                _age = getAgeToDspaly(_years, _months);                             
+            }
             return _age;
         }
 
         static string getAgeToDspaly(int years, int months)
         {
-            string _ageToDisplay = "";
-            if (years > 0)
-            {
-                _ageToDisplay = months > 0 ?
-                                years.ToString() + " Years " + months.ToString() + " Months" :
-                            years.ToString() + " Years";
-                                
-            }
-            else
-            {
-                _ageToDisplay = months > 0 ?
-                                "0 Years " + months.ToString() + " Months" :
-                                "Not Available";
-            }
+            string _yearsText, _monthsText;
+
+            _yearsText = years > 1 ? "Years" : "Year";
+            _monthsText = months > 1 ? "Months" : "Month"; 
+            string _ageToDisplay = months > 0 ?
+                                   string.Format("{0} {1} {2} {3}", years.ToString(), _yearsText , months.ToString(), _monthsText):
+                                   (years > 0 ? 
+                                        string.Format("{0} {1}", years.ToString(), _yearsText):
+                                        string.Format("{0} {1} {2} {3}", years.ToString(), _yearsText, months.ToString(), _monthsText));            
             return _ageToDisplay;
         }
+
+    }
+}
